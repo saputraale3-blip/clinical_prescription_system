07bf7c4773b5daa469782fd5170b82bc114f799e
Ale Saputra <saputraale3@gmail.com>
2026-05-25 12:58:34 +0800

    Built web
    Branch: main
    Commit: 00793aa36c072ca6e460843095057b81491f27d8 (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1143376455"
+    serviceWorkerVersion: "1736675290"
   }
 });
